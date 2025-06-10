b5d85a424f41c774551d5733d6954917be022e7f
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-10 15:20:24 +0700

    Add comments to clarify exception handling behavior

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -11,9 +11,13 @@ int main()
     }
     catch (int a)
     {
+        /*jika integer maka bloc ini akan dieksekusi*/
         cout << "Pernyataan akan dieksekusi" << endl; // penanda 2: menangkap exception
     }
     catch (...)
     {
+        /*jika selain integer maka bloc ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
     }
+    return 0;
 }
