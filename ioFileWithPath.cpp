bf23741fa378f85a2e0eb3893710d43eb9019a95
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-10 15:27:53 +0700

    Fix variable name consistency and enhance file writing logic

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,8 +5,29 @@ using namespace std;
 int main()
 {
     string baris;
-    string namaFile;
+    string NamaFile;
 
     cout << "Masukkan nama file: ";
-    cin >> namaFile;
+    cin >> NamaFile;
+
+    // Membuka file dalam mode menulis
+    ofstream outFile;
+    // Menunj
+    outFile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis File, \'q' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+
+        // Memeriksa apakah pengguna ingin keluar
+        if (baris == "q")
+        {
+            break; // Keluar dari loop jika pengguna memasukkan 'q'
+        }
+
+        // Menulis baris ke file
+        outFile << baris << endl;
+    }
 }
