e63f4b534939fb5fe7ea11c1170df59f40eb26eb
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-10 14:56:49 +0700

    point file

--- ioFile.cpp
+++ ioFile.cpp
@@ -20,10 +20,27 @@ int main()
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
-
+        // loop akan berhenti jika ada memasukkan karakter q
         if (baris == "q")
             break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl; // menulis ke file
     }
+    // selesai dalam menulis sekarang tutup filenya
     outfile.close(); // menutup file
-}
+    // Membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah nama file
+    infile.open("contohfile.txt");
+    cout << endl
+         << ">= Membukan dan Membaca File" << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // menampilkan setiap baris ke layar
+            cout << baris << '\n';
+        }
+    }
