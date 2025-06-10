98e4f1ba7f181339218a52a906037ce1ee954946
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-10 14:42:22 +0700

    Add initial implementation of file writing functionality

---
+++ ioFile.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    // Membuka file dalam mode menulis
+    ofstream outfile;
+    // Menunjuk ke sebuah nama file
+    outfile.open("data.txt");
+
+    cout << "Masukkan data (ketik 'exit' untuk keluar):" << endl;
+}
